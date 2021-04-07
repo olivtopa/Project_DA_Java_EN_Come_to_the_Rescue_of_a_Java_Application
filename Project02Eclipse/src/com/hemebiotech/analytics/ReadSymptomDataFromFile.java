7bd442b3167bd2391ecc65c44c58646e19dda3a5
Olivier <amoliv_825@hotmail.com>
2021-04-07 21:22:02 +0200

    les versions d'origine

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -15,9 +15,9 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 	private String filepath;
 	
 	/**
-	 * 
 	 * @param filepath a full or partial path to file with symptom strings in it, one per line
 	 */
+	
 	public ReadSymptomDataFromFile (String filepath) {
 		this.filepath = filepath;
 	}
